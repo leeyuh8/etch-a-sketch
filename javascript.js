dcc9e4d6f975fede8c8c7b4d9ea99c9bad365136
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-31 21:34:35 -0700

    Adjust canvas borders and default size

--- javascript.js
+++ javascript.js
@@ -6,8 +6,6 @@ let canvasSize;
 function requestCanvasSize() {
     canvasWidth = +prompt("Enter a number from 1-100 to set the canvas pixel width:");
     setCanvasSize();
-    // need to dynamically change canvas size by using value
-    // from requested canvas size to run setCanvasSize().
 }
 
 const canvas = document.querySelector(".canvas");
@@ -19,6 +17,8 @@ function setCanvasSize() {
         canvasPixel.addEventListener("mouseover", setPixelColor);
         canvas.appendChild(canvasPixel);
     };
+    // dynamically change size of pixel so that the requested 
+    // canvas size remains within the set default canvas div size.
 }
 
 
