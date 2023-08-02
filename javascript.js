752aaaa4ad5243e9e80e2ae3bf79a6a4acef781a
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-08-01 18:33:41 -0700

    Fix mouseover drawing function

--- javascript.js
+++ javascript.js
@@ -12,17 +12,19 @@ function requestCanvasSize() {
 const canvas = document.querySelector(".canvas");
 function setCanvasSize() {
     canvas.textContent = "";
-    console.log(typeof canvasPixelWidth);
     for (let i = 0; i < Math.pow(canvasWidth, 2); i++) {
         const canvasPixel = document.createElement('div');
         canvasPixel.classList.add("canvas-pixel");
-        canvasPixel.classList.add("pixel-color");
         canvasPixel.setAttribute("style", `width:${canvasPixelWidth}px; height:${canvasPixelWidth}px;`);
+        canvasPixel.addEventListener("mouseover", setPixelColor);
         canvas.appendChild(canvasPixel);
     };
     // - for canvas widths smaller than 16, dynamically make the 
     // pixels larger. and vise versa.
 }
 
+function setPixelColor () {
+    this.style.backgroundColor = "black";
+}
 
 
