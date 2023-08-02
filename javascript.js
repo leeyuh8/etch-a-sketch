b5f4da2b67539daf7bdf19adf1c071b51be50d43
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-08-01 18:28:38 -0700

    Adjust canvas size based on user request

--- javascript.js
+++ javascript.js
@@ -2,28 +2,27 @@
 
 const button = document.querySelector("button");
 button.addEventListener("click", requestCanvasSize);
-let canvasSize;
+let canvasPixelWidth;
 function requestCanvasSize() {
     canvasWidth = +prompt("Enter a number from 1-100 to set the canvas pixel width:");
+    canvasPixelWidth = 500 / canvasWidth;
     setCanvasSize();
 }
 
 const canvas = document.querySelector(".canvas");
 function setCanvasSize() {
     canvas.textContent = "";
+    console.log(typeof canvasPixelWidth);
     for (let i = 0; i < Math.pow(canvasWidth, 2); i++) {
         const canvasPixel = document.createElement('div');
         canvasPixel.classList.add("canvas-pixel");
-        canvasPixel.addEventListener("mouseover", setPixelColor);
+        canvasPixel.classList.add("pixel-color");
+        canvasPixel.setAttribute("style", `width:${canvasPixelWidth}px; height:${canvasPixelWidth}px;`);
         canvas.appendChild(canvasPixel);
     };
-    // dynamically change size of pixel so that the requested 
-    // canvas size remains within the set default canvas div size.
+    // - for canvas widths smaller than 16, dynamically make the 
+    // pixels larger. and vise versa.
 }
 
-
-function setPixelColor() {
-    this.setAttribute("style", "background-color: black");
-}
 
 
