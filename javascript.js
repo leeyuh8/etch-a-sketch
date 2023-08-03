db00fb9349d5a1a51155d8305cc094e020ae94c9
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-08-02 20:58:55 -0700

    Add drawShading function and unresponsive button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,6 @@
-let toolSelection = "Pencil";
-const tools = document.querySelectorAll(".tools > *");
-tools.forEach((tool) => tool.addEventListener("click", getTool));
-function getTool() {
-    toolSelection = this.value; 
-    console.log(toolSelection);
-}
 
+//DEFAULT SETTINGS
+let toolSelection = "Pencil";
 const canvas = document.querySelector(".canvas");
 function setDefaultCanvasSize() {
     for (let i = 0; i < Math.pow(16, 2); i++) {
@@ -13,15 +8,15 @@ function setDefaultCanvasSize() {
         canvasPixel.classList.add("canvas-pixel");
         canvasPixel.style.width = `${500/16}px`;
         canvasPixel.style.height = `${500/16}px`;
-        canvasPixel.addEventListener("mouseover", () => canvasPixel.style.backgroundColor = "black");
+        canvasPixel.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        canvasPixel.addEventListener("mouseover", drawShading);
         canvas.appendChild(canvasPixel);
     };
 }
 setDefaultCanvasSize();
 
 
-
-// BUTTONS
+// CANVAS SETTINGS
 const buttonPrompt = document.querySelector("button.prompt-canvas-size");
 buttonPrompt.addEventListener("click", requestCanvasSize);
 let canvasLength;
@@ -40,7 +35,8 @@ function setCanvasSize() {
             const canvasPixel = document.createElement('div');
             canvasPixel.classList.add("canvas-pixel");
             canvasPixel.setAttribute("style", `width:${canvasLength}px; height:${canvasLength}px;`);
-            canvasPixel.addEventListener("mouseover", drawPencil);
+            canvasPixel.style.backgroundColor = "rgba(0, 0, 0, 0)";
+            canvasPixel.addEventListener("mouseover", drawShading);
             canvas.appendChild(canvasPixel);
         };
     }
@@ -53,6 +49,16 @@ function clearCanvas() {
     clearPixels.forEach((pixel) => pixel.style.backgroundColor = "transparent");
 }
 
+
+
+//TOOL SETTINGS
+const tools = document.querySelectorAll(".tools > *");
+tools.forEach((tool) => tool.addEventListener("click", getTool));
+function getTool() {
+    toolSelection = this.value; 
+    console.log(toolSelection);
+}
+
 function drawPencil() {
     this.style.backgroundColor = "black";
 }
@@ -62,5 +68,14 @@ function drawRainbow() {
     let g = Math.floor((Math.random() * 256));
     let b = Math.floor((Math.random() * 256));
     this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function drawShading() {
+    let rgba = this.style.backgroundColor;
+    let rgbaArr = rgba.slice(5,-1)
+                      .replace(/ /g, '')
+                      .split(',');
+    let darkenAlpha = +rgbaArr[3] + .1;
+    this.style.backgroundColor = `rgba(${rgbaArr[0]}, ${rgbaArr[1]}, ${rgbaArr[2]}, ${darkenAlpha})`;
 }
 
