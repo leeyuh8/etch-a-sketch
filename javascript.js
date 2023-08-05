9e5556831c616b1d02f650fbb9374e56683a68f9
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-08-05 15:52:48 -0700

    Simplify code and functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+let selectedColorMode;
+let canvasItemsNodeList;
+
+const colorModes = document.querySelectorAll('.modes');
+colorModes.forEach((mode) => 
+    mode.addEventListener('click', (e) => selectedColorMode = `${e.target.value}`)
+);
+
 
 // CANVAS SIZE
 const canvasPromptButton = document.querySelector('.prompt-canvas-size');
@@ -14,82 +22,29 @@ function createCanvasItems(canvasRowLength) {
     for (let i = 0; i < Math.pow(canvasRowLength, 2); i++) {
         let canvasItem = document.createElement('div');
         canvasItem.classList.add('canvas-item');
-        canvasItem.style.backgroundColor = 'rgb(193, 193, 193)';
+        canvasItem.style.backgroundColor = 'rgb(255, 255, 255)';
         canvasItem.style.width = `${500/canvasRowLength}px`;
         canvasItem.style.height = `${500/canvasRowLength}px`;
         canvasContainer.appendChild(canvasItem);
     }
-};
 
-
-// COLOR MODE
-const colorModes = document.querySelectorAll('.modes');
-colorModes.forEach((mode) => 
-    mode.addEventListener('click', getColorMode));
-
-function getColorMode(e) {
-    let colorMode = `${e.target.value}`;
-
-    if (colorMode === 'Pencil') {
-        drawPencil();
-    } else if (colorMode === 'Rainbow') {
-        drawRainbow();
-    } else if (colorMode === 'Shading') {
-        drawShading();
-    }
-};
-
-function drawPencil() {
-    const canvasItemsNodeList = document.querySelectorAll('.canvas-item');
+    // to all items created, add mouseover event listener to run
+    // selected or default color mode.
+    canvasItemsNodeList = document.querySelectorAll('.canvas-item');
     canvasItemsNodeList.forEach((item) => 
-        item.addEventListener('mouseover', () => 
-            item.style.backgroundColor = 'rgb(0, 0, 0)'   // black
-        ));
-};
-
-function drawRainbow() {
-    const canvasItemsNodeList = document.querySelectorAll('.canvas-item');
-    canvasItemsNodeList.forEach((item) => 
-        item.addEventListener('mouseover', () => {
-            let r = Math.floor((Math.random() * 256));
-            let g = Math.floor((Math.random() * 256));
-            let b = Math.floor((Math.random() * 256));
-            item.style.backgroundColor = `rgb(${r}, ${g}, ${b})`; //random color
+        item.addEventListener('mouseover', (e) => {
+            if (selectedColorMode === 'Pencil') {
+                drawPencil(e);
+            } else if (selectedColorMode === 'Rainbow') {
+                drawRainbow(e);
+            } else if (selectedColorMode === 'Shading') {
+                drawShading(e);
+            } else {
+                drawPencil(e);
+            }
         })
-    );   
-};
-
-function drawShading() {
-    const canvasItemsNodeList = document.querySelectorAll('.canvas-item');
-    
-    canvasItemsNodeList.forEach((item) => {
-        let originalBg = window.getComputedStyle(item).getPropertyValue('background-color');
-        item.setAttribute('data-original-bg', `${originalBg}`);
-        }
-    )
-
-    canvasItemsNodeList.forEach((item) => 
-        item.addEventListener('mouseover', () => {
-            let originalBg = item.getAttribute('data-original-bg');
-            let originalBgArr = originalBg.slice(4,-1)
-                            .replace(/ /g, '')
-                            .split(',');
-            let tenPercentOfOriginalR = 0.1 * originalBgArr[0];
-            let tenPercentOfOriginalG = 0.1 * originalBgArr[1];
-            let tenPercentOfOriginalB = 0.1 * originalBgArr[2];
-
-            let currentBg = window.getComputedStyle(item).backgroundColor;
-            let currentBgArr = currentBg.slice(4,-1)
-                            .replace(/ /g, '')
-                            .split(',');
-            let darkenR = Math.floor(+currentBgArr[0] - tenPercentOfOriginalR);
-            let darkenG = Math.floor(+currentBgArr[1] - tenPercentOfOriginalG);
-            let darkenB = Math.floor(+currentBgArr[2] - tenPercentOfOriginalB);
-            item.style.backgroundColor = `rgb(${darkenR}, ${darkenG}, ${darkenB})`;  //darkened original color
-        })    
     );
 };
-// why when use rainbow or pencil first then click shading, shading does not work?
 
 
 // CANVAS SETTINGS
@@ -99,6 +54,29 @@ function clearCanvas() {
     let items = document.querySelectorAll(".canvas-container > *");
     items.forEach((item) => item.style.backgroundColor = "rgb(193, 193, 193, 193)");
 }
+
+
+// COLOR MODE
+function drawPencil(e) {
+    e.target.style.backgroundColor = 'rgba(0, 0, 0, 1)';   // black
+};
+
+function drawRainbow(e) {
+    let r = Math.floor((Math.random() * 256));
+    let g = Math.floor((Math.random() * 256));
+    let b = Math.floor((Math.random() * 256));
+    e.target.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 1)`; //random color
+};
+
+function drawShading(e) {
+    if (e.target.style.opacity === '') {
+        e.target.style.opacity = 0.9;
+    } else {
+        e.target.style.opacity -= 0.1;
+    }
+};
+
+
 
 
 
